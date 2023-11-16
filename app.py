d0a6920dedcb922ba26a9dc4e66ddee06654cade
ShreeshHunnur <131763615+ShreeshHunnur@users.noreply.github.com>
2023-11-16 08:33:45 +0000

    Added json api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template 
+from flask import Flask, jsonify, render_template
+
 
 
 app = Flask(__name__)
@@ -39,7 +40,9 @@ JOBS = [
 def hello_world():
   return render_template('home.html',jobs=JOBS,company_name='Jovian')
 
-
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
   
 if (__name__) == '__main__':
